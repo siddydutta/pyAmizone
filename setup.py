f14d94873c45bdae9e8d8415800cbdf78e51edff
Siddhartha Dutta <siddharthadutta@protonmail.com>
2020-08-04 20:20:12 +0530

    dependencies update

--- setup.py
+++ setup.py
@@ -21,4 +21,8 @@ setup(
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
+    install_requires = [
+        "beautifulsoup4 ~= 4.9.1",
+        "requests ~= 2.24.0",
+    ],
 )
